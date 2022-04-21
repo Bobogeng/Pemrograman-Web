a66192fff63f30644b5e82378345b420b4cb3d88
Bobogeng <suirito@gmail.com>
2022-04-21 18:17:50 +0700

    Fix

--- Semester 2/06_AdminLTE-3.1.0_PHP/sidebar.php
+++ Semester 2/06_AdminLTE-3.1.0_PHP/sidebar.php
@@ -1,7 +1,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="ti.php" class="brand-link">
+      <a href="index.php" class="brand-link">
           <img src="dist/img/logo-STT-NF-1.png" alt="AdminLTE Logo" class="brand-image" style="opacity: .8">
           <span class="brand-text font-weight-light">Nurul Fikri</span>
       </a>
@@ -45,7 +45,7 @@
                       </a>
                       <ul class="nav nav-treeview">
                           <li class="nav-item">
-                              <a href="ti.php" class="nav-link active">
+                              <a href="index.php" class="nav-link active">
                                   <i class="far fa-circle nav-icon"></i>
                                   <p>Teknik Informatika</p>
                               </a>
